0ccc53609e7b3e72845e53707af07c0145f170c8
Noda Toshitaka <junlongyetian361@gmail.com>
2022-02-24 10:35:58 +0000

    mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!,except: [:top, :about]
+  before_action :authenticate_user!,except: [:top, :about], if: :use_before_action?
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def use_before_action?
+    true
+  end
 
   protected
   
