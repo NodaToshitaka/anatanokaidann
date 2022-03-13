f673253da04d829ca610f6f8ea274457b0c960e4
Noda Toshitaka <junlongyetian361@gmail.com>
2022-03-13 12:29:57 +0000

    sort form change

--- app/controllers/sorts_controller.rb
+++ app/controllers/sorts_controller.rb
@@ -1,6 +1,7 @@
 class SortsController < ApplicationController
   def sort
     selection = params[:sort]
-    @stories = Story.sort(selection).page(params[:page])
+    stories = Story.sort(selection)
+    @stories = Kaminari.paginate_array(stories).page(params[:page])
   end
 end
